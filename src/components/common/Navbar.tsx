7cdef0e25a97831aa86888f22de00d7d4483415c
황민채 <trick0846@naver.com>
2025-09-21 11:32:20 +0900

    refactor: Navbar 로직 수정

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,85 +1,83 @@
 "use client";
 
+/**component */
 import Link from "next/link";
-import { TbAppleFilled } from "react-icons/tb";
+
+/**library */
 import { usePathname } from "next/navigation";
+import { ComponentType } from "react";
+
+/** icons */
+import type { IconType } from "react-icons/lib";
+import { ImPlay, ImPencil, ImHome, ImCog, ImCloud } from "react-icons/im";
+
+interface IconWrapperProps {
+  icon: ComponentType<{ className?: string; size?: number }>; // 이 타입에 대해서 제대로 모르는거 아닐까..??
+  size: number;
+  isActive?: boolean;
+}
+
+function IconWrapper({ icon: Icon, size, isActive }: IconWrapperProps) {
+  const iconClass = `aspect-square transition-colors duration-200 ${
+    isActive
+      ? "text-primary group-hover:text-primary-hover"
+      : "text-secondary group-hover:text-secondary-hover"
+  }`;
+  return <Icon size={size} className={iconClass} />;
+}
 
 type Page = {
   name: string;
   href: string;
-  icon: (isActive: boolean) => React.ReactNode;
+  icon: IconType;
 }[];
 
 export default function Navbar() {
   const pathname = usePathname();
-  const iconClass = "h-[35px] aspect-square";
+
   const pages: Page = [
     {
       name: "HOME",
       href: "/",
-      icon: (isActive) => (
-        <TbAppleFilled
-          size={35}
-          className={`${iconClass} ${isActive ? "text-primary hover:text-primary-hover" : "text-secondary"}`}
-        />
-      ),
+      icon: ImHome,
     },
     {
       name: "SQUARE",
       href: "/square",
-      icon: (isActive) => (
-        <TbAppleFilled
-          size={35}
-          className={`${iconClass} ${isActive ? "text-primary hover:text-primary-hover" : "text-secondary"}`}
-        />
-      ),
+      icon: ImCloud,
     },
     {
       name: "DREAM",
       href: "/dream/new",
-      icon: (isActive) => (
-        <TbAppleFilled
-          size={35}
-          className={`${iconClass} ${isActive ? "text-primary hover:text-primary-hover" : "text-secondary"}`}
-        />
-      ),
+      icon: ImPencil,
     },
     {
       name: "STREAM",
       href: "/stream",
-      icon: (isActive) => (
-        <TbAppleFilled
-          size={35}
-          className={`${iconClass} ${isActive ? "text-primary hover:text-primary-hover" : "text-secondary"}`}
-        />
-      ),
+      icon: ImPlay,
     },
     {
       name: "SETTINGS",
       href: "/settings",
-      icon: (isActive) => (
-        <TbAppleFilled
-          size={35}
-          className={`${iconClass} ${isActive ? "text-primary hover:text-primary-hover" : "text-secondary"}`}
-        />
-      ),
+      icon: ImCog,
     },
   ];
+
   return (
-    <nav className="flex h-[80px] justify-between gap-x-3 bg-white p-5 text-xs font-bold">
+    <nav className="flex h-[70px] justify-between gap-x-3 bg-white p-1 px-5 text-xs">
       {pages.map((page) => {
         const isActive = pathname == page.href;
         return (
           <Link
             href={page.href}
             key={page.name}
-            className={`group flex flex-col items-center justify-center`}
+            className="group flex flex-col items-center justify-center"
           >
-            {page.icon(isActive)}
+            <IconWrapper icon={page.icon} size={25} isActive={isActive} />
             <p
               className={
                 isActive
-                  ? `text-primary group-hover:text-primary-hover`
+                  ? "text-primary group-hover:text-primary-hover"
                   : "text-secondary group-hover:text-secondary-hover"
               }
             >
