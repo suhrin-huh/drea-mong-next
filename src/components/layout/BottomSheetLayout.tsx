c29ff4d0e26c3a0413597c8f14096e17144fa981
황민채 <trick0846@naver.com>
2025-10-01 01:56:13 +0900

    feat: BottomSheet 커스텀 props 적용

--- src/components/layout/BottomSheetLayout.tsx
+++ src/components/layout/BottomSheetLayout.tsx
@@ -1,11 +1,29 @@
-type BottomSheetLayoutProps = Readonly<{
-  children: React.ReactNode;
-}>;
+"use client";
 
-export default function BottomSheetLayout({ children }: BottomSheetLayoutProps) {
+// type
+import { HTMLAttributes, ReactNode } from "react";
+
+interface BottomSheetLayoutProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+  isDarkMode?: boolean;
+}
+
+export default function BottomSheetLayout({
+  children,
+  className,
+  isDarkMode = false,
+  ...rest
+}: BottomSheetLayoutProps) {
   return (
-    <div className={"p-lg gap-md flex min-h-0 flex-1 flex-col rounded-t-xl bg-white"}>
+    <div
+      className={`p-lg gap-md flex min-h-0 flex-1 flex-col rounded-t-xl ${
+        isDarkMode ? "bg-gray-800 text-white" : "bg-white"
+      } ${className ?? ""}`}
+      {...rest}
+    >
       {children}
     </div>
   );
 }
+
+// TODO: type 정의 이후에 다시 고민해보기
