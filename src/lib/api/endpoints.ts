647c96982f4fbbf1810be7497e0146c852ba8e76
황민채 <trick0846@naver.com>
2025-10-02 11:03:09 +0900

    feat: endpoints.ts 생성

---
+++ src/lib/api/endpoints.ts
@@ -0,0 +1,10 @@
+// API 경로
+
+export const API_BASE_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api`;
+
+export const endpoints = {
+  auth: {
+    login: "/auth/kakao/login",
+  },
+  users: {},
+};
