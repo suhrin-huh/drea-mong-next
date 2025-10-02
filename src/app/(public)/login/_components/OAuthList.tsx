7550967feb3b3fd3db9db6b116aa252b92ee0c09
황민채 <trick0846@naver.com>
2025-10-02 11:06:57 +0900

    feat: kakao login 로직 연결

--- src/app/(public)/login/_components/OAuthList.tsx
+++ src/app/(public)/login/_components/OAuthList.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 // component
 import Button from "@/components/common/Button";
 import { IoChatbubble } from "react-icons/io5";
 
 export default function OAuthList() {
+  const handleKaKaoLogin = () => {
+    window.location.href = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/auth/kakao/login`;
+  };
+
   return (
     <div className="flex h-[300px] flex-col justify-center">
       <Button
@@ -11,6 +17,7 @@ export default function OAuthList() {
         size="full"
         rounded="md"
         className="flex h-[50px] items-center rounded-lg bg-yellow-300 p-2"
+        onClick={handleKaKaoLogin}
       >
         <IoChatbubble size={30} />
         <p className="flex-1 text-base font-bold">카카오 로그인</p>
