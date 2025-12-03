6ed64a04cd19addfc81ee0650994f4df867727be
황민채 <trick0846@naver.com>
2025-12-03 16:42:10 +0900

    feat: endpoints 수정 및 추가

--- src/lib/api/endpoints.ts
+++ src/lib/api/endpoints.ts
@@ -1,11 +1,35 @@
-// API 경로
+export const API_BASE_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1`; // 이거 버전을 명명해야하나?
 
-export const API_BASE_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api`;
+// Auth
+export const LOGIN = () => `/auth/login`;
+export const LOGOUT = () => ``;
 
-export const endpoints = {
-  auth: {
-    login: "/auth/kakao/login",
-    logout: "/auth/logout",
-  },
-  users: {},
-};
+// Oauth
+export const OAUTH_LOGIN = (provider: string) => `/auth/oauth/${provider}`;
+export const OAUTH_LOGOUT = (provider: string) => `/auth/oauth/${provider}/callback`;
+
+// User
+export const GET_MY_PROFILE = () => `/users/me`;
+export const PATCH_NICKNAME = () => `/users/me/nickname`;
+
+// Dream
+export const GET_MY_DREAM_LIST = () => `/dreams`;
+export const CREATE_DREAM = () => `/dreams`;
+export const GET_MY_DREAM_DETAIL = (dreamId: number) => `/dreams/${dreamId}`;
+export const UPDATE_DREAM = (dreamId: number) => `/dreams/${dreamId}`;
+export const DELETE_DREAM = (dreamId: number) => `/dreams/${dreamId}`;
+
+// AI Functions
+export const GENERATE_IMAGE = () => `/dreams/image`;
+export const GENERATE_INTERPRETATION = () => `/dreams/interpretation`;
+
+// Shared Dreams
+export const GET_SHARED_DREAM_LIST = () => `/dreams/shared`;
+export const GET_SHARED_DREAM_DETAIL = (dreamId: number) => `/dreams/shared/${dreamId}`;
+
+// Comments
+export const CREATE_COMMENT = (dreamId: number) => `/dreams/${dreamId}/comments`;
+export const DELETE_COMMENT = (dreamId: number, commentId: number) =>
+  `/dreams/${dreamId}/comments/${commentId}`;
+
+// export const  = () => ``;
