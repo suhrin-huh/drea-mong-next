b73584a800d187e86dfd64788279408b1581c013
황민채 <trick0846@naver.com>
2025-12-18 00:04:25 +0900

    style: queries.ts 코드 포맷팅

--- src/lib/api/queries.ts
+++ src/lib/api/queries.ts
@@ -2,11 +2,13 @@
 // import httpClient from "./fetcher/http-client";
 import { serverFetcher } from "./fetcher/server-fetcher";
 
-import { GET_MY_PROFILE,
+import {
+  GET_MY_PROFILE,
   GET_MY_DREAM_LIST,
   GET_MY_DREAM_DETAIL,
   GET_SHARED_DREAM_LIST,
-  GET_SHARED_DREAM_DETAIL, } from "./endpoints";
+  GET_SHARED_DREAM_DETAIL,
+} from "./endpoints";
 
 // types
 import {
@@ -15,7 +17,7 @@ import {
   SharedDreamList,
   SharedDreamDetail,
   DreamId,
-  Profile
+  Profile,
 } from "@/types";
 
 // TODO: 여기에서 처리를 fetcher<Profile>로 하기보다는 fetch<ApiResponse<Profile>>이 나은가..?
@@ -25,14 +27,14 @@ import {
 /**
  * 유저 정보 조회 - 서버 전용
  */
-export async function  getMyProfileServer () {
+export async function getMyProfileServer() {
   return serverFetcher<Profile>(GET_MY_PROFILE());
 }
 /**
  * 꿈 조회 - 서버 전용
  * @params year, month
  */
-export async function  getMyDreamListServer (year: number, month: number) {
+export async function getMyDreamListServer(year: number, month: number) {
   const params = new URLSearchParams({ year: String(year), month: String(month) });
   return serverFetcher<MyDreamList>(`${GET_MY_DREAM_LIST()}?${params}`);
 }
@@ -40,20 +42,20 @@ export async function  getMyDreamListServer (year: number, month: number) {
  * 꿈 상세 조회 - 서버 전용
  * @params dreamId
  */
-export async function  getMyDreamDetailServer (dreamId: DreamId) {
+export async function getMyDreamDetailServer(dreamId: DreamId) {
   return serverFetcher<MyDreamDetail>(GET_MY_DREAM_DETAIL(dreamId));
 }
 /**
  * 공유된 꿈 목록 조회 - 서버 전용
  */
-export async function  getSharedDreamListServer () {
+export async function getSharedDreamListServer() {
   return serverFetcher<SharedDreamList>(GET_SHARED_DREAM_LIST());
 }
 /**
  * 공유된 꿈 상세 조회 - 서버 전용
  * @params dreamId
  */
-export async function  getSharedDreamDetailServer (dreamId: DreamId) {
+export async function getSharedDreamDetailServer(dreamId: DreamId) {
   return serverFetcher<SharedDreamDetail>(GET_SHARED_DREAM_DETAIL(dreamId));
 }
 
