ceb4c48ee0b6dccf2c63f256aa906de3a61e0d2a
황민채 <trick0846@naver.com>
2026-01-03 00:01:03 +0900

    feat: 푸시 알림 옵션 제거

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -6,17 +6,10 @@ import { useState } from "react";
 // component
 import BottomSheetLayout from "@/components/layout/BottomSheetLayout";
 import SettingsOption from "./_components/SettingsOption"; // TODO: SettingsOption 수정 필요!
-import ToggleButton from "@/components/common/ToggleButton";
 import Modal from "@/components/common/Modal";
 import Button from "@/components/common/Button";
 
 export default function SettingsPage() {
-  const [pushEnabled, setPushEnabled] = useState(false);
-
-  const handlePushEnabled = () => {
-    setPushEnabled((prev) => !prev);
-  };
-
   const [modalEnabled, setModalEnabled] = useState(false);
 
   const openModal = () => {
@@ -37,11 +30,6 @@ export default function SettingsPage() {
       <BottomSheetLayout>
         {/* settings 옵션 리스트 */}
         <SettingsOption label="닉네임 변경" onClick={openModal} />
-        <SettingsOption
-          label="푸시 알림 활성화"
-          icon={<ToggleButton checked={pushEnabled} onChange={() => {}} />}
-          onClick={handlePushEnabled}
-        />
         <SettingsOption label="취침시간 설정하기" onClick={openModal} />
         <SettingsOption label="로그아웃" onClick={handleLogout} />
         {/* 모달 */}
